9a648701c060e46600e42fe9b449d3d975f91e4a
Haroenv <fingebimus@me.com>
2015-08-14 11:12:36 +0200

    comments

--- Height.java
+++ Height.java
@@ -1,23 +1,33 @@
 class Height {
-	static double h;					//the height at that angle
-	static double d;
-	static double DISTANCE = 50;	//50m is the distance away from launch
-	static double HEIGHT = 10;
-	static double alphaRad;				//that angle in rad
-	static double alphaDeg;				//angle is also shown in degrees
+	static double h, height, d, distance;
+	static double DISTANCE = 50;		//50m is the distance away from launch
+	static double HEIGHT = 10;			//10m is the minimum height
+	static double alphaDeg, alphaRad;	//the angle to analyze (will go from 0-90°)
 	static final double MINANG = 0;		//minimum angle in deg
 	static final double MAXANG = 90;	//max angle in deg
 	public static void main(String[] args) {
+		/**
+		 * If there are two arguments given, then use those as
+		 * <ul>
+		 * 	<li>the distance from which you want to know a certain angle's height</li>
+		 * 	<li>the height you want to know the distance at a certain angle</li>
+		 * </ul>
+		 * Defaults to 50 and 10.
+		 * Displays all degrees from 0-90 with 5 degree steps
+		 */
 		if (args.length == 2) {
-			DISTANCE = Double.parseDouble(args[0]);
-			HEIGHT = Double.parseDouble(args[1]);
+			distance = Double.parseDouble(args[0]);
+			height = Double.parseDouble(args[1]);
+		} else {
+			distance = DISTANCE;
+			height = HEIGHT;
 		}
 		for (double i = MINANG; i < MAXANG; i += 5) {
 			alphaDeg = i;
 			alphaRad = alphaDeg*Math.PI/180;
-			h = Math.tan(alphaRad)*DISTANCE; // tan(alpha) * d = h
-			d = HEIGHT/Math.tan(alphaRad);
-			System.out.println("angle: "+alphaDeg+"°\t | height at "+DISTANCE+"m: "+Math.round(100*h)/100+"m\t | distance for "+HEIGHT+"m: "+Math.round(100*d)/100+"m");
+			h = Math.tan(alphaRad)*distance; // tan(alpha) * d = h
+			d = height/Math.tan(alphaRad);
+			System.out.println("angle: "+alphaDeg+"°\t | height at "+distance+"m: "+Math.round(100*h)/100+"m\t | distance for "+height+"m: "+Math.round(100*d)/100+"m");
 		}
 	}
 }
