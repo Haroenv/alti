44723bc458e327ef446c52d0b8df7c28c1b67c8d
Haroenv <fingebimus@me.com>
2015-08-21 21:36:26 +0200

    no timeout

--- alti.js
+++ alti.js
@@ -10,14 +10,14 @@ function init() {
   if (window.DeviceOrientationEvent) {
     window.addEventListener('deviceorientation', function(eventData) {
       var alpha = eventData.beta;
-      window.setTimeout(deviceOrientationHandler(alpha), 1000);
+      print(alpha);
     }, false);
   } else {
     document.getElementById("fail").style.display = "inline";
   }
 }
 
-function deviceOrientationHandler(alpha) {
+function print(alpha) {
   document.getElementById("angle").innerHTML = Math.round(alpha);
   h.innerHTML = Math.round(Math.tan(Math.radians(Math.round(alpha))) * d.value * 10) / 10;
 }
