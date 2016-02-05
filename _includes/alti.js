d2d0a8a8c3d28b1053456bbf2635e98fadc78522
Haroen Viaene <hello@haroen.me>
2016-02-05 15:23:02 +0100

    use jekyll to include files and minify the output

---
+++ _includes/alti.js
@@ -0,0 +1,21 @@
+var h = document.getElementById('height');
+var d = document.getElementById('distance');
+init();
+Math.radians = function(degrees) {
+  return degrees * Math.PI / 180;
+};
+function init() {
+  if (window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(eventData) {
+      print(eventData.beta);
+    }, false);
+  }
+}
+function print(angle) {
+  document.getElementById("angle").innerHTML = Math.round(angle);
+  h.innerHTML = Math.round(Math.tan(Math.radians(Math.round(angle))) * d.value * 10) / 10;
+}
+function save() {
+  document.getElementById('saved').innerHTML = h.innerHTML;
+}
+document.getElementById('save').addEventListener('click',save());
